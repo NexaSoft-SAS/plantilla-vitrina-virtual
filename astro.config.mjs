42a2f915cca3645cd2f7f0aa0fbc664875c1525f
Maikpwwq <mariasf@correo.udistrital.edu.co>
2024-05-08 13:36:40 -0500

    fixed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
+// import vercel from "@astrojs/vercel/serverless";
 // const isProd = process.env.NODE_ENV === "production";
 
 
@@ -14,6 +14,6 @@ export default defineConfig({
   })],
   site: "https://nexasoft.github.io",
   base: "/astro-showcase",
-  output: "hybrid",
-  adapter: vercel()
+  output: "static",
+  // adapter: vercel()
 });
